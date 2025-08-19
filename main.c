306f4cbf50e5f5074e88691a2b5d54b7afa5bf91
Caleb <cszachariah@gmail.com>
2025-08-19 13:13:31 -0400

    Changed the program to remove the newline sequence at the end of the triangle name.

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
 
 int main() {
-  printf("Hello World!");
+  int length;
+  int width;
+
+  char triag_name[25]; //When creating an array of any type in C, you must specify a size for the array.
+
+  
+  printf("What is the name of your triangle?");
+  fgets(triag_name, 25, stdin); //fgets is used to include whitespace in user input. Arguments: fgets(variable_name, variable_size, Input/Output)
+  triag_name[strlen(triag_name) - 1] = '\0'; // Removes the \n escape character. Syntax: variable_name[strlen(variable_name) -1] = '\0'.
+                                   // \0 is the escape sequence for nothing.
+
+
+
+  printf("What is the width of the triangle?:");
+  scanf("%d", &width);
+
+  printf("What is the height of the triangle?:");
+  scanf("%d", &length);
+
+  int hyp = sqrt( pow(length, 2) + pow(width, 2) );
+
+  printf("Here is the length of the hypotenuse for triangle %s: %d",triag_name, hyp);
+
+  return 0;
 }
